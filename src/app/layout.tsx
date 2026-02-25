9a2ae0c3868fea14a1bd3cd544cfa6b526508ec4
Andrei <66415921+Andreihbk@users.noreply.github.com>
2026-02-25 19:52:27 +0200

    Remove commented CustomCursor component
    Removed commented-out CustomCursor component from layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-         /* <CustomCursor />*/
+
         {children}
       </body>
     </html>
